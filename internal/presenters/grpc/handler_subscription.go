11b2cbae9accf315d6180c2f6a8600785736f283
aerosystems <art_k0st@icloud.com>
2025-01-06 01:51:14 +0200

    implement gRPC methods

--- internal/presenters/grpc/handler_subscription.go
+++ internal/presenters/grpc/handler_subscription.go
@@ -3,8 +3,7 @@ package GRPCServer
 import (
 	"context"
 	"github.com/aerosystems/subscription-service/internal/common/protobuf/subscription"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -19,9 +18,16 @@ func NewSubscriptionHandler(subscriptionUsecase SubscriptionUsecase) *Subscripti
 	}
 }
 
-func (sh SubscriptionHandler) CreateFreeTrialSubscription(context.Context, *subscription.CreateFreeTrialSubscriptionRequest) (*subscription.CreateFreeTrialSubscriptionResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateFreeTrialSubscription not implemented")
+func (sh SubscriptionHandler) CreateFreeTrialSubscription(ctx context.Context, req *subscription.CreateFreeTrialSubscriptionRequest) (*subscription.CreateFreeTrialSubscriptionResponse, error) {
+	sub, err := sh.subscriptionUsecase.CreateFreeTrial(req.CustomerUuid)
+	if err != nil {
+		return nil, err
+	}
+	return &subscription.CreateFreeTrialSubscriptionResponse{
+		SubscriptionUuid: sub.Uuid.String(),
+	}, nil
 }
-func (sh SubscriptionHandler) DeleteSubscription(context.Context, *subscription.DeleteSubscriptionRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DeleteSubscription not implemented")
+
+func (sh SubscriptionHandler) DeleteSubscription(ctx context.Context, req *subscription.DeleteSubscriptionRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, sh.subscriptionUsecase.DeleteSubscription(uuid.MustParse(req.SubscriptionUuid))
 }
