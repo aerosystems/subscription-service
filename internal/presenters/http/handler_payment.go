834bc1fbd587f01ce5e00ddcfaa74ef2b9394e9d
aerosystems <art_k0st@icloud.com>
2025-01-16 01:42:37 +0200

    make checking nil dependencies

--- internal/presenters/http/handler_payment.go
+++ internal/presenters/http/handler_payment.go
@@ -9,6 +9,12 @@ func NewPaymentHandler(
 	baseHandler *BaseHandler,
 	paymentUsecase PaymentUsecase,
 ) *PaymentHandler {
+	if baseHandler == nil {
+		panic("baseHandler is required")
+	}
+	if paymentUsecase == nil {
+		panic("paymentUsecase is required")
+	}
 	return &PaymentHandler{
 		BaseHandler:    baseHandler,
 		paymentUsecase: paymentUsecase,
